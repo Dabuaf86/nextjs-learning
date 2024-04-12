cf8800e609d1bab106ab245f246001c9f7f99564
Daniel-Dataka <Daniel.a@data-ka.com>
2024-04-12 17:22:42 -0300

    login/logout + metadata added

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,14 @@
+import { Metadata } from 'next'
 import { Suspense } from 'react';
 import { lusitana } from '@/app/ui/fonts';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+
+export const metadata: Metadata = {
+    title: 'Dashboard'
+}
 
 export default async function Page() {
 
